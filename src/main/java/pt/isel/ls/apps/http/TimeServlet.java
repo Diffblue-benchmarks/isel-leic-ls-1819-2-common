504d177b41da1a02e0f62fc8876c5207d476da52
Pedro Felix <pmhsfelix@gmail.com>
2019-05-21 21:55:56 +0100

    starts using Maven dependencies

--- src/main/java/pt/isel/ls/apps/http/TimeServlet.java
+++ src/main/java/pt/isel/ls/apps/http/TimeServlet.java
@@ -21,9 +21,9 @@ public class TimeServlet extends HttpServlet {
         System.out.println(req.getHeader("Accept"));
 
         Charset utf8 = Charset.forName("utf-8");
-        resp.setContentType(String.format("text/plain; charset=%s", utf8.name()));
+        resp.setContentType(String.format("text/html; charset=%s", utf8.name()));
 
-        String respBody = String.format("Current date and time is %s", Instant.now());
+        String respBody = String.format("<h1>Current date and time is %s</h1>", Instant.now());
         byte[] respBodyBytes = respBody.getBytes(utf8);
         resp.setStatus(200);
         resp.setContentLength(respBodyBytes.length);
--- src/main/java/pt/isel/ls/html/HtmlText.java
+++ src/main/java/pt/isel/ls/html/HtmlText.java
@@ -3,7 +3,7 @@ package pt.isel.ls.html;
 import java.io.IOException;
 import java.io.Writer;
 
-import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import pt.isel.ls.common.Writable;
 
 public class HtmlText implements Writable {
@@ -16,6 +16,6 @@ public class HtmlText implements Writable {
 
     @Override
     public void writeTo(Writer w) throws IOException {
-        w.write(StringEscapeUtils.escapeHtml(_text));
+        w.write(StringEscapeUtils.escapeHtml4(_text));
     }
 }
